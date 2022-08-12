ec3c125f29e455375d2bc334ad9e316290c7e34a
omaran bazna <omran4d@gmail.com>
2022-08-12 17:08:17 -0400

    import Login int the main App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Login from "./components/Login";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
