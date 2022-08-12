fa8e97496f119184c5d27d9c2c61cd3e28929394
omaran bazna <omran4d@gmail.com>
2022-08-12 17:15:19 -0400

    styling the nav component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 const Login = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Nav>
+        <a href="">
+          <img src="/images/login-logo.svg" />
+        </a>
+      </Nav>
+    </Container>
+  );
 };
+const Container = styled.div`
+  padding: 0;
+`;
 
+const Nav = styled.nav`
+  max-width: 1128px;
+  margin: 0;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flew-wrap: nowrap;
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 76px) {
+      padding: 0 5px;
+    }
+  }
+`;
 export default Login;
