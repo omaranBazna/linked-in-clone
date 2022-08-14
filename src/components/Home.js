a6d3d924be329634e67cfad41328acd3cb650c2e
omaran bazna <omran4d@gmail.com>
2022-08-13 22:24:03 -0400

    resposive layout section

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,5 +55,13 @@ const Layout = styled.div`
   display: grid;
   grid-template-areas: "leftside main righside";
   grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  grid-template-rows: auto;
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 export default Home;
