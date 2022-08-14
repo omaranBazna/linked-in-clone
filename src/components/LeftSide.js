609ffd23d19d5cf6fc80506da328f6a36fbf6a76
omaran bazna <omran4d@gmail.com>
2022-08-13 22:27:25 -0400

    add some content to Leftside component

---
+++ src/components/LeftSide.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import React from "react";
+
+const LeftSide = () => {
+  return <Container>Left side</Container>;
+};
+const Container = styled.div`
+  grid-area: leftside;
+`;
+export default LeftSide;
