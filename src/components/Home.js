d805e83338d5c577eca9b64b229ea2adba2f3e4e
omaran bazna <omran4d@gmail.com>
2022-08-13 22:12:46 -0400

    styling the home content;

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,13 +14,27 @@ const Home = () => {
   );
 };
 const Container = styled.div`
-  background-color: white;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
-  left: 0;
-  padding: 0 24px;
-  position: fixed;
-  top: 0;
-  width: 100vw;
-  z-index: 100;
+  padding-top: 52px;
+  max-width: 100%;
+`;
+const Content = styled.div`
+  max-width: 1128px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+const Section = styled.div`
+  min-height: 50px;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+  h5 {
+    color: #0a660a;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
 `;
 export default Home;
