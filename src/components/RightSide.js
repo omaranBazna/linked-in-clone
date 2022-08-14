2a1521dabaa41d3ac3958e28e2d2990b1e38f29e
omaran bazna <omran4d@gmail.com>
2022-08-14 01:17:36 -0400

    add  Follow Card Component to the right side

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -3,9 +3,20 @@ import styled from "styled-components";
 import React from "react";
 
 const RightSide = () => {
-  return <Container>Right side</Container>;
+  return (
+    <Container>
+      <FollowCard>rightside</FollowCard>
+    </Container>
+  );
 };
 const Container = styled.div`
   grid-area: rightside;
+`;
+const FollowCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
 `;
 export default RightSide;
