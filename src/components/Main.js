675aee7f55ef2e87c44166eae2fb45cfef36aff0
omaran bazna <omran4d@gmail.com>
2022-08-13 22:28:10 -0400

    add some content to main component

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import React from "react";
+
+const Main = () => {
+  return <Container>Left side</Container>;
+};
+const Container = styled.div`
+  grid-area: main;
+`;
+export default Main;
