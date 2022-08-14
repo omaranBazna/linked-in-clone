38109157d767eeba54dc740793c4132fd91d961e
omaran bazna <omran4d@gmail.com>
2022-08-14 10:58:25 -0400

    add comment card to be used in the main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,4 +8,15 @@ const Main = () => {
 const Container = styled.div`
   grid-area: "main";
 `;
+
+const CommentsCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 3px rgba(0, 0, 0, 0.15), 0 0 0 rgba(0, 0, 0, 0.2);
+`;
 export default Main;
