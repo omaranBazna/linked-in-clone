905fe04dafc7eaba39b7819b56ccb6f5bbcb2bca
omaran bazna <omran4d@gmail.com>
2022-08-14 01:21:13 -0400

    add Title to the follow card

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -5,7 +5,12 @@ import React from "react";
 const RightSide = () => {
   return (
     <Container>
-      <FollowCard>rightside</FollowCard>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" />
+        </Title>
+      </FollowCard>
     </Container>
   );
 };
@@ -23,4 +28,5 @@ const FollowCard = styled.div`
   box-shadow: 0 0 0 1px rgb(0, 0, 0, 0.65), 0 0 0 rgb(0, 0, 0, 0.2);
   padding: 12px;
 `;
+const Title = styled.div``;
 export default RightSide;
