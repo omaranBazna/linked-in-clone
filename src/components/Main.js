9b21e65767d333a376b13f8bd55c68cc1766a906
omaran bazna <omran4d@gmail.com>
2022-08-15 11:37:22 -0400

    add post modal to teh main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import React from "react";
+import PostModal from "./PostModal";
 
 const Main = () => {
   return (
@@ -87,6 +88,7 @@ const Main = () => {
             </button>
           </SocialAction>
         </Article>
+        <PostModal />
       </div>
     </Container>
   );
--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,8 +1,16 @@
-import { StyledComponent } from "styled-components";
+import styled from "styled-components";
 import React from "react";
 
 const PostModal = () => {
-  return <div></div>;
+  return <Container>post modal</Container>;
 };
 
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 10000;
+`;
 export default PostModal;
