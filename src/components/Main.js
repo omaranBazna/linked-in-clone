87b0a2c7e1412290c108d4e84054f0f720beb026
omaran bazna <omran4d@gmail.com>
2022-08-14 20:09:50 -0400

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -39,7 +39,9 @@ const Main = () => {
                 <span>Info</span>
                 <span>Date</span>
               </div>
-              <button></button>
+              <button>
+                <img src="/images/widget-icon.svg" />
+              </button>
             </a>
           </SharedActor>
         </Article>
@@ -127,5 +129,51 @@ const Article = styled(CommentCard)`
   margin: 0 0 8px;
   overflow: visible;
 `;
-const SharedActor = styled.div``;
+const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  botton {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+  }
+`;
 export default Main;
