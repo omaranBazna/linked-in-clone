ee4b2ccf55cfc50054259859a4c40ab43c8d785d
omaran bazna <omran4d@gmail.com>
2022-08-15 11:40:16 -0400

    styling the post modal

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -2,7 +2,11 @@ import styled from "styled-components";
 import React from "react";
 
 const PostModal = () => {
-  return <Container>post modal</Container>;
+  return (
+    <Container>
+      <Content></Content>
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -12,5 +16,12 @@ const Container = styled.div`
   right: 0;
   bottom: 0;
   z-index: 10000;
+  color: black;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+const Content = styled.div`
+  width: 100%;
+  max-width: 552px;
+  background-color: white;
 `;
 export default PostModal;
